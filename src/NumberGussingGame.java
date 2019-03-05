8a19f1641e41485969191384f4d20eb9425f80a0
Ricky <richard.anderson.2021@student.lhschools.org>
2019-03-05 14:18:13 -0500

    Adding a continue

--- src/NumberGussingGame.java
+++ src/NumberGussingGame.java
@@ -11,6 +11,7 @@ keyboard=new Scanner (System.in);
 System.out.println("-------------------------------------");
 System.out.println("|Welcome To The Number Guessing Game|");
 System.out.println("|       Please Pick A Number        |");
+System.out.println("|        Between 1 and 100          |");
 System.out.println("-------------------------------------");
 
 
@@ -20,7 +21,8 @@ do {
     numberGuesser = Float.valueOf(keyboard.nextLine());
     if (numberGuesser == numberPicker) {
         System.out.println("you've guessed the number,you must be a psychic");
-        break;
+        System.out.println("Would you like to continue");
+        System.out.println("101 = yes 102 = no");
     }
     if(numberGuesser > numberPicker){
         System.out.println("You're too high");
